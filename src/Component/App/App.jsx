7229fae45f6ecc799ae8f56c0fd03939257ebf04
jyoti-2 <jyoti02.nitt@gmail.com>
2022-04-23 13:01:34 +0530

    Updated Home Page

---
+++ src/Component/App/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Home from './Home/Home.jsx';
+import '/App.css';
+
+
+const App = () => {
+    return(
+        <div className="app-container">
+            Horrb
+            <Home/>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/Component/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";;
+import './Home.css';
+
+const Home = () =>{
+    return(
+        <div className="home-container">
+            I am Home page
+        </div>
+    );
+};
+
+export default Home;
